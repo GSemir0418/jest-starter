89f66c0b2607d8327b08c78dce1eb54f7f01d583
GSemir0418 <845217811@qq.com>
2022-11-25 14:39:56 +0800

    feat: setupFiles mock localStoage

--- jest.config.js
+++ jest.config.js
@@ -123,7 +123,7 @@ module.exports = {
   // runner: "jest-runner",
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
+  setupFiles: ["./tests/jest-setup.ts"],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   // setupFilesAfterEnv: [],
@@ -135,7 +135,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  // testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
---
+++ tests/jest-setup.ts
@@ -0,0 +1,19 @@
+// 给测试环境mock一个全局localStorage实现
+Object.defineProperty(global, "localStorage", {
+  value: {
+    store: {} as Record<string, string>,
+    setItem(key: string, value: string) {
+      this.store[key] = value;
+    },
+    getItem(key: string) {
+      return this.store[key];
+    },
+    removeItem(key: string) {
+      delete this.store[key];
+    },
+    clear() {
+      this.store = {};
+    },
+  },
+  configurable: true,
+});

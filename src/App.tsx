3a3f400d14109feb0db49cb95694ca458031727d
GSemir0418 <845217811@qq.com>
2022-11-29 18:19:32 +0800

    feat: User Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import AuthButton from "components/AuthButton";
+import User from "components/User";
 
 const App = () => {
   return (
     <div>
       <section>
         <AuthButton>登录</AuthButton>
+      </section>
+      <section>
+        <User />
       </section>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);

d725dadc79c2c8ae9b2e7ff642dbdd6f59a7e440
GSemir0418 <845217811@qq.com>
2022-11-29 17:41:42 +0800

    feat: redux toolkit init

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -9,3 +9,13 @@ export interface GetUserRoleRes {
 export const getUserRole = async () => {
   return axios.get<GetUserRoleRes>("https://whatever/site/api/role");
 };
+
+// 获取用户列表
+export interface FetchUserRes {
+  id: string;
+  name: string;
+}
+
+export const fetchUser = async () => {
+  return axios.get<FetchUserRes>("https://whatever/site/api/users");
+};
